aef715ba9ee06632d834196164b55bacdaecb870
asukita <pvc38@hotmail.com>
2013-06-19 02:03:02 -0500

    intend que guarda las imágenes en galeria y se muestran en el imageview

--- src/com/blastic/pawhub_petmatch/EditUserActivity.java
+++ src/com/blastic/pawhub_petmatch/EditUserActivity.java
@@ -1,7 +1,17 @@
 package com.blastic.pawhub_petmatch;
 
+import java.io.BufferedInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
+import android.media.MediaScannerConnection.MediaScannerConnectionClient;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
@@ -13,7 +23,7 @@ import android.widget.ImageView;
 public class EditUserActivity extends Activity {
 
 	ImageButton btnFinish;
-	ImageView picUSer;
+	ImageView picUser;
 
 	String name = "";
 
@@ -26,10 +36,10 @@ public class EditUserActivity extends Activity {
 		setContentView(R.layout.activity_edit_user);
 
 		btnFinish = (ImageButton) findViewById(R.id.btnEditAPet);
-		ImageView picUSer = (ImageView) findViewById(R.id.userPicture);
-		name = Environment.getExternalStorageDirectory() + "/DCIM/test.jpg";
+		picUser = (ImageView) findViewById(R.id.userPicture);
+		name = Environment.getExternalStorageDirectory().getAbsolutePath()
+				+ "/DCIM/Camera/test.jpg";
 
-		Log.i("file", name);
 	}
 
 	@Override
@@ -45,6 +55,89 @@ public class EditUserActivity extends Activity {
 		startActivity(editPetIntend);
 	}
 
-	
+	public void onBtnUserPic_Click(View v) {
 
+		/*Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		int code = TAKE_PICTURE;
+
+		Uri output = Uri.fromFile(new File(name));
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, output);
+
+		Log.i("INTENTD", intent.toString());
+
+		startActivityForResult(intent, code);*/
+		
+		Intent pickIntent = new Intent();
+		pickIntent.setType("image/*");
+		pickIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+		Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+		String pickTitle = "Select or take a new Picture"; // Or get from strings.xml
+		Intent chooserIntent = Intent.createChooser(pickIntent, pickTitle);
+		chooserIntent.putExtra
+		(
+		  Intent.EXTRA_INITIAL_INTENTS, 
+		  new Intent[] { takePhotoIntent }
+		);
+
+		startActivityForResult(chooserIntent, TAKE_PICTURE);
+
+	}
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	
+		
+		if (requestCode == TAKE_PICTURE) {
+			if (data != null) {
+				/**
+				 * En el caso de una vista previa, obtenemos el extra “data” del
+				 * intent y lo mostramos en el ImageView
+				 */
+				if (data.hasExtra("data")) {
+					picUser.setImageBitmap((Bitmap) data.getParcelableExtra("data"));
+				}
+				/**
+				 * De lo contrario es una imagen completa
+				 */
+			} else {
+				/**
+				 * A partir del nombre del archivo ya definido lo buscamos y creamos
+				 * el bitmap para el ImageView
+				 */
+					picUser.setImageBitmap(BitmapFactory.decodeFile(name));
+				/**
+				 * Para guardar la imagen en la galería, utilizamos una conexión a
+				 * un MediaScanner
+				 */
+				new MediaScannerConnectionClient() {
+					private MediaScannerConnection msc = null;
+					{
+						msc = new MediaScannerConnection(getApplicationContext(),
+								this);
+						msc.connect();
+					}
+
+					public void onMediaScannerConnected() {
+						msc.scanFile(name, null);
+					}
+ 
+					public void onScanCompleted(String path, Uri uri) {
+						msc.disconnect();
+					}
+				};
+			}/**
+			* Recibimos el URI de la imagen y construimos un Bitmap a partir de un stream de Bytes
+			*/
+			     } else if (requestCode == SELECT_PICTURE){
+			     Uri selectedImage = data.getData();
+			     InputStream is;
+			     try {
+			     is = getContentResolver().openInputStream(selectedImage);
+			     BufferedInputStream bis = new BufferedInputStream(is);
+			     Bitmap bitmap = BitmapFactory.decodeStream(bis);
+			     picUser.setImageBitmap(bitmap);	
+			     } catch (FileNotFoundException e) {}
+			     }		
+	}
 }
