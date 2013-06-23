dd83be104b5883e182bd7bf2beaaafadc12ac7fa
Israel Gomez <is_gom@live.com.mx>
2013-06-22 18:35:32 -0600

    Solucioné el problema de Tabs con Fragments En la clase MainTabsActivity agrego los Tabs, aquí hay que poner el Fragment al que van a asociar esa pestaña

--- src/com/blastic/pawhub_petmatch/MainTabsActivity.java
+++ src/com/blastic/pawhub_petmatch/MainTabsActivity.java
@@ -2,17 +2,24 @@ package com.blastic.pawhub_petmatch;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
 
 public class MainTabsActivity extends FragmentActivity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-//        Fragment fragment = new RateMyPetActivity();
-        
-//        if(savedInstanceState == null){
-//        	fragment.getFragmentManager().beginTransaction().add(android.R.id.tabs, fragment, "FRAGMENT").commit();
-//        }	
-    }
+	private FragmentTabHost mTabHost;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.tabs_layout);
+
+	 mTabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);
+	    mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+	    mTabHost.addTab(mTabHost.newTabSpec("tab1").setIndicator("Rate a Pet"),
+	            RateMyPetActivityFragment.class, null);
+	    mTabHost.addTab(mTabHost.newTabSpec("tab2").setIndicator("View My Rates"),
+	    		RateMyPetActivityFragment.class, null);
+	    mTabHost.addTab(mTabHost.newTabSpec("tab3").setIndicator("Global Rates"),
+	    		RateMyPetActivityFragment.class, null);
+	}
 }
